0a88e57c84eb9ade57120a1bf3a3d56224d8dbc7
mzperezous <mzperezous@gmail.com>
2022-07-12 18:35:24 -0400

    Handle directories in Retrosheet ingestion

--- data-populator/retro/download-yearly-files.sh
+++ data-populator/retro/download-yearly-files.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
+START_YEAR=1995
+END_YEAR=2013 # TODO: 2022
 
-for YEAR in {1915..2021}
+# Handle directories
+if [ ! -d .data ]; 
+then 
+    mkdir .data
+else
+    rm -rf .data
+    mkdir .data
+# TODO: Handle other possible cases
+fi
+
+if [ ! -d tmp ]; 
+then 
+    mkdir tmp 
+else
+    rm -rf tmp
+    mkdir tmp
+fi
+
+for (( YEAR = ${START_YEAR}; YEAR <= ${END_YEAR}; YEAR++ ))
 do
     wget https://www.retrosheet.org/events/${YEAR}eve.zip -O tmp/${YEAR}eve.zip
     rm -rf .data/${YEAR} # TODO: Remove
