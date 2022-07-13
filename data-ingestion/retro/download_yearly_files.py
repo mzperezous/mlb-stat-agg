900b3bc3c415717c3dc81bd8810a7fe624256613
mzperezous <mzperezous@gmail.com>
2022-07-12 21:44:17 -0400

    Unzip EVE files by default to .data directories

--- data-ingestion/retro/download_yearly_files.py
+++ data-ingestion/retro/download_yearly_files.py
@@ -6,8 +6,46 @@
 
 import os
 import requests
+from zipfile import ZipFile
+
 
 def download_year_eve(year):
-    cwd = os.getcwd()
-    basename = f'{cwd}/{year}eve.zip'
-    return basename
+
+    thisdir = os.path.dirname(os.path.abspath(__file__))
+    basename = f'{year}eve.zip'
+
+    tmpdir = f'{thisdir}/tmp'
+    filename = f'{tmpdir}/{basename}'
+    if not os.path.isfile(tmpdir):
+        os.makedirs(tmpdir, exist_ok=True)
+    url = f'https://www.retrosheet.org/events/{basename}'
+
+    datadir = f'{thisdir}/.data'
+
+    try:
+        # Download the ZIP
+        response = requests.get(url)
+        open(filename, 'wb').write(response.content)
+
+        # Unzip to ./.data/<year>
+        year_dir = f'{datadir}/{year}'
+        if os.path.isfile(year_dir):
+            os.rmdir(year_dir)
+        with ZipFile(filename, "r") as zip_ref:
+            zip_ref.extractall(year_dir)
+
+        return True
+    except Exception as e:
+        print(e)
+        print(f'Couldn\'t download .eve file for {year}')
+        return False
+
+
+def download_year_range_eve(start_year, end_year):
+
+    for year in range(start_year, end_year + 1):
+        success = download_year_eve(year)
+
+        if not success:
+            print(f'Failed downloading {year} EVE.')
+
--- data-ingestion/retro/test/test_download_yearly_files.py
+++ data-ingestion/retro/test/test_download_yearly_files.py
@@ -1,7 +1,45 @@
-from download_yearly_files import download_year_eve
+import download_yearly_files
 import os
 
+
 def test_download_year_eve():
-    output = download_year_eve(2005)
-    cwd = os.getcwd()
-    assert output == f'{cwd}/2005eve.zip'
+    outdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+    tmpdir = f'{outdir}/tmp'
+    filename = f'{outdir}/tmp/2005eve.zip'
+
+    datadir = f'{outdir}/.data/2005'
+
+    # Handle tmp and data dirs
+    if os.path.isfile(filename):
+        os.remove(filename)
+    else:
+        if not os.path.isfile(tmpdir):
+            os.makedirs(tmpdir, exist_ok=True)
+
+    if not os.path.isfile(datadir):
+        os.makedirs(datadir, exist_ok=True)
+
+    # Run test
+    download_yearly_files.download_year_eve(2005)
+
+    # Check for file
+    assert os.path.isfile(filename)
+
+
+def test_download_and_process_years():
+    outdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+    datadir = f'{outdir}/.data'
+
+    for i in range(2005, 2013):
+        year_dir = f'{datadir}/{i}'
+        if os.path.isfile(year_dir):
+            os.remove(year_dir)
+
+    download_yearly_files.download_year_range_eve(2005, 2012)
+    for i in range(2005, 2013):
+        year_dir = f'{datadir}/{i}'
+        print(year_dir)
+        assert os.path.exists(year_dir)
+
