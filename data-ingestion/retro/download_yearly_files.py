5f2bf2ad4ba290ceba0027baf7ff2ba228cbf126
mzperezous <mzperezous@gmail.com>
2022-07-13 20:43:21 -0400

    Add entry point to RetroSheet download script

--- data-ingestion/retro/download_yearly_files.py
+++ data-ingestion/retro/download_yearly_files.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python3
 """
     Download and unzip year-by-year files from RetroSheet.
 
     Data: Historical game event level
 """
 
+import sys
+import getopt
 import os
 import requests
 from zipfile import ZipFile
@@ -23,6 +26,7 @@ def download_year_eve(year):
     datadir = f'{thisdir}/.data'
 
     try:
+        print(f'Downloading EVE from {year}...')
         # Download the ZIP
         response = requests.get(url)
         open(filename, 'wb').write(response.content)
@@ -48,4 +52,30 @@ def download_year_range_eve(start_year, end_year):
 
         if not success:
             print(f'Failed downloading {year} EVE.')
+
+
+if __name__ == "__main__":
+    argv = sys.argv[1:]
+    start = 1999
+    end = 2021
+
+    try:
+        options, args = getopt.getopt(argv, 'f:l', ['first=', 'last='])
+
+        for opt, arg in options:
+            if opt in ('-f', '--first'):
+                start = int(arg)
+            elif opt in ('-l', '--last'):
+                end = int(arg)
+
+        download_year_range_eve(start, end)
+        sys.exit(0)
+
+    except getopt.GetoptError:
+        error_message = "Usage: download_yearly_files.py [-f | --first year1] [-l | --last year2]\n"
+        error_message += "\t-f or --first\n\t\t year1: First year to pull\n"
+        error_message += "\t-l or --last\n\t\t year2: Last year to pull"
+        print(error_message)
+
+        sys.exit(1)
 
